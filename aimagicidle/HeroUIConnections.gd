be45f2ed0f72ead3d5e25c41587ba4e7a520b61d
Deimantx <deimantasdicm@gmail.com>
2025-07-03 12:49:31 +0300

    new hero panel

--- aimagicidle/HeroUIConnections.gd
+++ aimagicidle/HeroUIConnections.gd
@@ -4,14 +4,16 @@ extends Control
 # You can update the slot names and connections as needed.
 
 func _draw():
-    var grid = get_parent().get_node("EquipmentGrid")
-    # Example: Draw a line from Weapon to Armor slot
-    var weapon_slot = grid.get_node("Slot_Weapon")
-    var armor_slot = grid.get_node("Slot_Armor")
-    var weapon_pos = weapon_slot.get_global_rect().position + weapon_slot.get_global_rect().size / 2
-    var armor_pos = armor_slot.get_global_rect().position + armor_slot.get_global_rect().size / 2
-    draw_line(to_local(weapon_pos), to_local(armor_pos), Color(0.7, 0.7, 0.8), 2)
-    # Add more draw_line calls for other connections as needed
+	var grid = get_parent().get_node("EquipmentGrid")
+	# Example: Draw a line from Weapon to Armor slot
+	var weapon_slot = grid.get_node("Slot_Weapon")
+	var armor_slot = grid.get_node("Slot_Armor")
+	var weapon_pos = weapon_slot.get_global_rect().position + weapon_slot.get_global_rect().size / 2
+	var armor_pos = armor_slot.get_global_rect().position + armor_slot.get_global_rect().size / 2
+	var local_weapon_pos = weapon_pos - global_position
+	var local_armor_pos = armor_pos - global_position
+	draw_line(local_weapon_pos, local_armor_pos, Color(0.7, 0.7, 0.8), 2)
+	# Add more draw_line calls for other connections as needed
 
 func _ready():
-    update() # Redraw when ready 
+	call_deferred("update") # Redraw when ready 
